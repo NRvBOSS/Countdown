48fc4c983e9a4e10360b4c978d682814edfc14e7
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2026-07-08 11:12:09 +0400

    add calendar

--- app/src/main/java/com/example/countdown/MainActivity.java
+++ app/src/main/java/com/example/countdown/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.countdown;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,6 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,9 +25,17 @@ public class MainActivity extends AppCompatActivity {
         TextView tvTitle = findViewById(R.id.tvTitle);
         Button btnSelectedDate = findViewById(R.id.btnSelectDate);
         btnSelectedDate.setOnClickListener(v ->{
-            tvTitle.setText("Countdown App");
+            Calendar calendar = Calendar.getInstance();
 
-        });
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+            int second = calendar.get(Calendar.SECOND);
+
+            new DatePickerDialog(this, (view, year1, month1, dayOfMonth) -> {}, year, month, day).show();
+            Log.d("Countdown", "Current: " + year + "/" + (month + 1) + "/" + day);      });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
