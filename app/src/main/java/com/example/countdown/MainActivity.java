55194e228dbc588e96b0d75f8b2e337eb7afe976
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2026-07-08 10:25:09 +0400

    add button

--- app/src/main/java/com/example/countdown/MainActivity.java
+++ app/src/main/java/com/example/countdown/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.countdown;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TextView tvTitle = findViewById(R.id.tvTitle);
+        Button btnSelectedDate = findViewById(R.id.btnSelectDate);
+        btnSelectedDate.setOnClickListener(v ->{
+            tvTitle.setText("Countdown App");
+
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
